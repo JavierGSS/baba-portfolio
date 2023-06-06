4696c8a87c93a73121b1c645f105d7694c3573e9
baba <jagasal@icloud.com>
2023-06-06 01:05:31 -0600

    footer added script relocated

--- scripts.js
+++ scripts.js
@@ -34,3 +34,13 @@ function move() {
 }
 
 setInterval(move, 2);
+
+const displayTime = document.querySelector(".display-time");
+// Time
+function showTime() {
+  let time = new Date();
+  displayTime.innerText = time.toLocaleTimeString("en-US", { hour12: false });
+  setTimeout(showTime, 1000);
+}
+
+showTime();
